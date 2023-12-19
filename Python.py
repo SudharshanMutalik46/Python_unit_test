4b672f2492962c0a703b5c2b9581dc5c56ef1d8d
Sudharshan Mutalik <114343463+SudharshanMutalik46@users.noreply.github.com>
2023-12-19 10:35:55 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,15 +1,28 @@
-import pytest
-from app import app
+import unittest
+import requests
 
+class TestATGWorldWebsite(unittest.TestCase):
+    
+    def test_website_availability(self):
+        """
+        Test that the ATG World website is available and loads properly.
+        """
+        url = 'https://atg.world'
+        
+        try:
+            response = requests.get(url)
+            # Print statements to log activity
+            print(f"Attempting to connect to {url}")
+            
+            # The website is considered up if the status code is 200
+            self.assertEqual(response.status_code, 200)
+            print("Connection successful. Website is up.")
+            
+        except requests.RequestException as e:
+            # If any exception occurs during the request, the test fails
+            self.fail(f"Website could not be reached. Error: {e}")
+            print("Connection failed. Website is down.")
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-
-
-def test_app_is_working(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hello World!" in response.data
+# This allows the test to be run from the command line
+if __name__ == '__main__':
+    unittest.main()
